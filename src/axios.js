fb42d1d467f3d43f45c7855e2bee8b1ac16c06aa
Belousov Vitaliy <vitaliy.jc@yandex.ru>
2023-04-30 22:15:21 +0500

    first commit

---
+++ src/axios.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+
+const REACT_APP_SERVER_API = process.env.REACT_APP_SERVER_API;
+
+const instance = axios.create({
+  baseURL: `${REACT_APP_SERVER_API}`,
+});
+
+instance.interceptors.request.use((config) => {
+  config.headers.Authorization = window.localStorage.getItem('token');
+  return config;
+});
+
+export default instance;
